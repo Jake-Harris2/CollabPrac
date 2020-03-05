bdc7ac21f3dfdca9f0cf90c05666273f0ee127a3
Jake-Harris2 <44767481+Jake-Harris2@users.noreply.github.com>
2020-03-05 00:23:38 +0000

    Imported Project

---
+++ collabtest/Assets/Scripts/cameraTurn.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraTurn : MonoBehaviour
+{
+    public bool canTurnLeft;
+    public bool canTurnRight;
+    private float rotateWall = 0.2f;
+    private float rotation;
+    public Rigidbody camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Rigidbody>();
+        canTurnLeft = true;
+        canTurnRight = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rotation = camera.transform.rotation.y;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //if (canTurnLeft == true)
+            //{
+            //    transform.Rotate(new Vector3(0, -1, 0) * 30f * Time.deltaTime);
+            //    canTurnRight = true;
+            //}
+            TurnLeft();
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //if (canTurnRight == true)
+            //{
+            //    transform.Rotate(new Vector3(0, 1, 0) * 30f * Time.deltaTime);
+            //    canTurnLeft = true;
+            //}
+
+            TurnRight();
+        }
+
+        if (rotation >= rotateWall)
+        {
+            canTurnRight = false;
+        }
+
+        if (rotation <= -rotateWall)
+        {
+            canTurnLeft = false;
+        }
+    }
+
+    public void TurnLeft()
+    {
+        if (canTurnLeft == true)
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * 30f * Time.deltaTime);
+            canTurnRight = true;
+        }
+    }
+
+    public void TurnRight()
+    {
+        if (canTurnRight == true)
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * 30f * Time.deltaTime);
+            canTurnLeft = true;
+        }
+    }
+}
---
+++ collabtest/Assets/Scripts/rightButtonClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightButtonClick : MonoBehaviour
+{
+    public GameObject camera;
+    public cameraTurn turn;
+
+    private void Start()
+    {
+        turn = camera.GetComponent<cameraTurn>();
+    }
+
+    public void ClickRight()
+    {
+        turn.TurnRight();
+    }
+}
